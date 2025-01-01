9d19571a15321761ca2bb9c69bc50add40f50a0b
YEnchev <ysenchev@gmail.com>
2025-01-01 17:06:28 +0200

    new commit

---
+++ 2. JS Front-End - June 2024/3. JS Arrays and Strings - Lab/01-sum-first-and-last-array-elements.js
@@ -0,0 +1,9 @@
+
+function sumFirstAndLastArrayElements(input) {
+    let first = input[0];
+    let last = input[input.length - 1];
+
+    console.log(first + last);
+}
+
+sumFirstAndLastArrayElements([10, 17, 22, 33]);
