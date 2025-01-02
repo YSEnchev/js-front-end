f6bee37f844d85f39ec329a3197a6b0fe9091f64
YEnchev <ysenchev@gmail.com>
2025-01-03 00:45:44 +0200

    substring and censored wors

---
+++ 2. JS Front-End - June 2024/3. JS Arrays and Strings - Lab/04-substring.js
@@ -0,0 +1,7 @@
+function substring(text, firstNumber, count) {
+
+    const result = text.substring(firstNumber, firstNumber + count);
+    console.log(result); 
+}
+
+substring('ASentence', 1, 8);
---
+++ 2. JS Front-End - June 2024/3. JS Arrays and Strings - Lab/05-censored-words.js
@@ -0,0 +1,16 @@
+function censoredWords(text, replaceWord) {
+
+    const replacedText = text.replaceAll(replaceWord, '*'.repeat(replaceWord.length));
+    console.log(replacedText);
+
+}
+
+// function solveWithoutReplaceAll(text, word) {
+//     while (text.includes(word)) {
+//         text = text.replace(word, '*'.repeat(word.length))
+//     }
+//     console.log(text);
+
+// }
+
+censoredWords('hidden Find the hidden word', 'hidden');
