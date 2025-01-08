fe70048a1bd1825529899ac7477e7cc83b7c81be
YEnchev <ysenchev@gmail.com>
2025-01-09 01:21:25 +0200

    ex 6

--- 2. JS Front-End - June 2024/06-modern-times-of-#(HashTag).js
+++ 2. JS Front-End - June 2024/06-modern-times-of-#(HashTag).js
@@ -1,4 +1,21 @@
-function modernTimesOfHashTag() {}
+function modernTimesOfHashTag(inputString) {
+  const regex = /#[a-zA-z]+/gm;
+
+  const matches = inputString.matchAll(regex);
+  for (const match of matches) {
+    console.log(match[0].substring(1));
+  }
+
+  //   inputString.split(/\s/g).forEach((word) => {
+  //     const result = regex.exec(word);
+
+  //     if (result?.length > 0) {
+  //       [match] = result;
+
+  //       console.log(match.substring(1));
+  //     }
+  //   });
+}
 
 modernTimesOfHashTag(
   'The symbol # is known #variously in English-speaking #regions as the #number sign'
