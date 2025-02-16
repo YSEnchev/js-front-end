113e47240cd68c184840c863c6a318c59e9924f9
YEnchev <ysenchev@gmail.com>
2025-02-17 00:05:05 +0200

    object exer

--- 2. JS Front-End - June 2024/8. Objects and Classes - Exercises/05-inventory.js
+++ 2. JS Front-End - June 2024/8. Objects and Classes - Exercises/05-inventory.js
@@ -1,4 +1,21 @@
-function solve(inputArr) {}
+function solve(inputArr) {
+  inputArr
+    .map((heroData) => {
+      const [heroName, heroLevel, Items] = heroData.split(' / ');
+
+      return {
+        heroName,
+        heroLevel,
+        Items,
+      };
+    })
+    .sort((a, b) => a.heroLevel - b.heroLevel)
+    .forEach((heroOutput) => {
+      console.log(`Hero: ${heroOutput.heroName}`);
+      console.log(`level => ${heroOutput.heroLevel}`);
+      console.log(`items => ${heroOutput.Items}`);
+    });
+}
 
 solve([
   'Batman / 2 / Banana, Gun',
