25d873ae64bd8d01478c7724e2f6c9b95e948e12
YEnchev <ysenchev@gmail.com>
2025-02-28 00:12:57 +0200

    DOM

--- 2. JS Front-End - June 2024/9. DOM Introduction - Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
+++ 2. JS Front-End - June 2024/9. DOM Introduction - Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/02.Collect List Items/CollectListItems.js
@@ -2,11 +2,13 @@ function extractText() {
   const ulElements = document.getElementById('items');
   const resultElement = document.getElementById('result');
 
-  const textResult = ulElements.textContent
-    .split('\n')
-    .map((row) => row.trim())
-    .join('\n')
-    .trim();
+  //   const textResult = ulElements.textContent
+  //     .split('\n')
+  //     .map((row) => row.trim())
+  //     .join('\n')
+  //     .trim();
+
+  textResult = ulElements.innerText;
 
   resultElement.textContent = textResult;
 }
