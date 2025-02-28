aa182775f44b2f1ffe2dd0fcfc8265578f0418e3
YEnchev <ysenchev@gmail.com>
2025-03-01 00:45:15 +0200

    update DOM

--- 2. JS Front-End - June 2024/9. DOM Introduction - Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
+++ 2. JS Front-End - June 2024/9. DOM Introduction - Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/06.Sum Table/SumTable.js
@@ -1,3 +1,27 @@
 function sumTable() {
+  //   const resultElement = document.getElementById('sum');
 
+  //   const trElements = document.querySelectorAll('table tbody tr');
+  //   let sum = 0;
+
+  //   for (const trElement of trElements) {
+  //     const secondElement = trElement.children[1];
+
+  //     if (secondElement.tagName === 'TD' && secondElement.id !== 'sum') {
+  //       const price = Number(secondElement.textContent);
+  //       sum += price;
+  //     }
+  //   }
+
+  //   resultElement.textContent = sum;
+
+  const resultElement = document.getElementById('sum');
+  const tdElements = document.querySelectorAll(
+    'table tr td:nth-child(2):not(#sum)'
+  );
+
+  resultElement.textContent = Array.from(tdElements).reduce(
+    (sum, td) => sum + Number(td.textContent),
+    0
+  );
 }
