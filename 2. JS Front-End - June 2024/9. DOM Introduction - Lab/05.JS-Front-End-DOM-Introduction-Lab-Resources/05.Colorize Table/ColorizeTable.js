0577754f390cf6c445f138370f4106575e0dc0b9
YEnchev <ysenchev@gmail.com>
2025-03-01 23:31:36 +0200

    update DOM

--- 2. JS Front-End - June 2024/9. DOM Introduction - Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
+++ 2. JS Front-End - June 2024/9. DOM Introduction - Lab/05.JS-Front-End-DOM-Introduction-Lab-Resources/05.Colorize Table/ColorizeTable.js
@@ -1,16 +1,16 @@
 function colorize() {
-  const tableElement = document.querySelector('table tbody');
-  const tableRowElements = tableElement.children;
+  // const tableElement = document.querySelector('table tbody');
+  // const tableRowElements = tableElement.children;
 
-  for (let i = 0; i < tableRowElements.length; i += 2) {
-    tableRowElements[i].style.backgroundColor = 'teal';
+  // for (let i = 0; i < tableRowElements.length; i += 2) {
+  //   tableRowElements[i].style.backgroundColor = 'teal';
+  // }
+
+  const evenTableRowElements = document.querySelectorAll(
+    'table tbody tr:nth-child(odd)'
+  );
+
+  for (const trElement of evenTableRowElements) {
+    trElement.style.backgroundColor = 'teal';
   }
-
-  //   const evenTableRowElements = document.querySelectorAll(
-  //     'table tbody tr:nth-child(odd)'
-  //   );
-
-  //   for (const trElement of evenTableRowElements) {
-  //     trElement.style.backgroundColor = 'teal';
-  //   }
 }
