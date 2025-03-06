9efb9183d5adb07c36ec4aec159f71f8fab569ab
YEnchev <ysenchev@gmail.com>
2025-03-06 09:03:25 +0200

    DOM

--- 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
+++ 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/01. Subtraction/subtract.js
@@ -1,3 +1,10 @@
 function subtract() {
-    console.log('TODO:...');
+  const [firstInput, secondInput] =
+    document.querySelectorAll('input[type="text"]');
+
+  console.log(firstInput.value);
+  console.log(secondInput.value);
+
+  document.getElementById('result').textContent =
+    Number(firstInput.value) - Number(secondInput.value);
 }
