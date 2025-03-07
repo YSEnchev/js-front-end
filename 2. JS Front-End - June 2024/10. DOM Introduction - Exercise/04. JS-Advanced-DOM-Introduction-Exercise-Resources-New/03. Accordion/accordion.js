b21c9169841823bc69afcd0a3825817747cec312
YEnchev <ysenchev@gmail.com>
2025-03-07 18:03:01 +0200

    DOM

--- 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
+++ 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/03. Accordion/accordion.js
@@ -1,18 +1,15 @@
 function toggle() {
-  const buttonElement = document.getElementsByClassName('button');
-  //   const buttonElement = document.querySelector('.head span.button');
-
-  const extraElement = document.getElementById('extra');
-  //   const extraElement = document.querySelector('#extra');
+  const buttonElement = document.querySelector('.head span.button');
+  const extraElement = document.querySelector('#extra');
 
   const { display } = extraElement.style;
   const isHidden = display === 'none';
 
   if (!display || isHidden) {
     extraElement.style.display = 'block';
-    buttonElement.textContent = 'LESS';
+    buttonElement.textContent = 'Less';
   } else {
     extraElement.style.display = 'none';
-    buttonElement.textContent = 'MORE';
+    buttonElement.textContent = 'More';
   }
 }
