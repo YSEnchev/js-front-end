ccb7917eea5d122a0c419342d3ae521fcf4d5ad7
YEnchev <ysenchev@gmail.com>
2025-03-07 14:42:39 +0200

    DOM

--- 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
+++ 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,26 @@
 function solve() {
-  //TODO...
+  const validNamingConventions = ['Camel Case', 'Pascal Case'];
+
+  const textInputElement = document.querySelector('#text');
+  const namingConventionElement = document.querySelector('#naming-convention');
+
+  const resultElement = document.querySelector('.result-container #result');
+
+  if (!validNamingConventions.includes(namingConventionElement.value)) {
+    resultElement.textContent = 'Error!';
+    return;
+  }
+
+  const pascalCaseText = textInputElement.value
+    .toLowerCase()
+    .split(' ')
+    .map((firstLetter) =>
+      firstLetter[0].toUpperCase().concat(firstLetter.slice(1))
+    )
+    .join('');
+
+  resultElement.textContent =
+    namingConventionElement.value === validNamingConventions[0]
+      ? pascalCaseText[0].toLowerCase().concat(pascalCaseText.slice(1))
+      : pascalCaseText;
 }
