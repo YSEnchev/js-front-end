59d8fc43edc61be748e8dbaee007db31320e513d
YEnchev <ysenchev@gmail.com>
2025-03-10 23:27:17 +0200

    DOM

--- 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
+++ 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/06. Format the Text/solution.js
@@ -1,3 +1,16 @@
 function solve() {
-  //TODO
+  const textAreaElement = document.getElementById('input');
+  const outputAreaElement = document.getElementById('output');
+
+  function createParagraphElement(text) {
+    const pElement = document.createElement('p');
+    pElement.textContent = text;
+    return pElement;
+  }
+
+  const sentence = textAreaElement.value
+    .split('. ')
+    .map((x) => x.trim().concat('.'));
+
+  console.log(sentence);
 }
