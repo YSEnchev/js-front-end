2f4c8c59c4f714545fe7d7e8aab75294ce7522c0
YEnchev <ysenchev@gmail.com>
2025-03-12 22:47:14 +0200

    DOM

--- 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -1,8 +1,29 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+  document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+  const inputTextArea = document.querySelector('#inputs textarea');
+  const outputBestRestaurants = document.querySelector(
+    '#outputs #bestRestaurant p'
+  );
+  const workersElement = document.querySelector('#outputs #workers p');
+
+  function onClick() {
+    const restaurants = JSON.parse(inputTextArea.value).map((data) => {
+      const [restaurantName, workerData] = data.split(' - ');
+
+      const workers = workerData.split(', ').map((workerData) => {
+        const [name, salary] = workerData.split(' ');
+        return {
+          name,
+          salary: Number(salary),
+        };
+      });
+      return {
+        restaurantName,
+        workers,
+      };
+    });
+
+    console.log(restaurants);
+  }
 }
