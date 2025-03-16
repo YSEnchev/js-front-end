668859b99bae1a3ac21794b346f49aa50a57fc44
YEnchev <ysenchev@gmail.com>
2025-03-16 14:21:26 +0200

    DOM

--- 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
+++ 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/07. Hell's Kitchen/app.js
@@ -25,11 +25,32 @@ function solve() {
         };
       }
 
-      acc[restaurantName].workers.concat(workers);
+      acc[restaurantName].workers.push(...workers);
 
       return acc;
     }, {});
 
-    console.log(restaurants);
+    function getAvgSalary(restaurantData) {
+      return (
+        restaurantData.workers.reduce((a, b) => a + b.salary, 0) /
+        restaurantData.workers.length
+      );
+    }
+
+    const [bestRestaurantKey] = Object.keys(restaurants).sort(
+      (a, b) => getAvgSalary(restaurants[b]) - getAvgSalary(restaurants[a])
+    );
+
+    const bestWorkers = restaurants[bestRestaurantKey].workers
+      .slice()
+      .sort((a, b) => b.salary - a.salary);
+
+    outputBestRestaurants.textContent = `Name: ${bestRestaurantKey} Average Salary: ${getAvgSalary(
+      restaurants[bestRestaurantKey]
+    ).toFixed(2)} Best Salary: ${bestWorkers[0].salary.toFixed(2)}`;
+
+    workersElement.textContent = bestWorkers
+      .map((x) => `Name: ${x.name} With Salary: ${x.salary}`)
+      .join(' ');
   }
 }
