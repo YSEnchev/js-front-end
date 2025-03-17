8df8b160b85ffc7fb3856d0a12547cef058c5ad4
YEnchev <ysenchev@gmail.com>
2025-03-17 22:37:33 +0200

    DOM

--- 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ 2. JS Front-End - June 2024/10. DOM Introduction - Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,17 @@
 function generateReport() {
-    //TODO
+  const thElements = document.querySelectorAll('thead tr th');
+  const tbodyRows = document.querySelectorAll('tbody tr');
+
+  const checkedInputElements = [...thElements]
+    .map((x, i) => ({ input: x.children[0], index: i }))
+    .filter((x) => x.input.checked);
+
+  const outputData = [...tbodyRows].map((tr) => {
+    return checkedInputElements.reduce((acc, curr) => {
+      acc[curr.input.name] = tr.children[curr.index].textContent;
+      return acc;
+    }, {});
+  });
+
+  document.querySelector('#output').value = JSON.stringify(outputData);
 }
