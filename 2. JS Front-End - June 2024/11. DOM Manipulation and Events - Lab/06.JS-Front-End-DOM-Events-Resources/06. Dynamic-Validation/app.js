0a06d47a6d9953c486258201e4cef7b922dbae5d
YEnchev <ysenchev@gmail.com>
2025-03-23 22:26:17 +0200

    DOM

--- 2. JS Front-End - June 2024/11. DOM Manipulation and Events - Lab/06.JS-Front-End-DOM-Events-Resources/06. Dynamic-Validation/app.js
+++ 2. JS Front-End - June 2024/11. DOM Manipulation and Events - Lab/06.JS-Front-End-DOM-Events-Resources/06. Dynamic-Validation/app.js
@@ -1,7 +1,19 @@
 function validate() {
   const emailInputElement = document.getElementById('email');
+  const pattern = /[a-z]+@[a-z]+\.[a-z]+/;
 
   emailInputElement.addEventListener('change', (e) => {
-    console.log(e.currentTarget.value);
+    //FIRST SOLUTION
+    // if (pattern.test(e.currentTarget.value)) {
+    //   e.currentTarget.classList.remove('error');
+    // } else {
+    //   e.currentTarget.classList.add('error');
+    // }
+
+    //SECOND SOLUTION
+    if (!pattern.test(e.currentTarget.value)) {
+      return e.currentTarget.classList.add('error');
+    }
+    e.currentTarget.classList.remove('error');
   });
 }
