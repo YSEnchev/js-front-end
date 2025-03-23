cdfb7ba05c3b4c1a63852f9a47dabee9a0419d6b
YEnchev <ysenchev@gmail.com>
2025-03-23 15:14:43 +0200

    DOM

--- 2. JS Front-End - June 2024/11. DOM Manipulation and Events - Lab/06.JS-Front-End-DOM-Events-Resources/03. Add-Delete/app.js
+++ 2. JS Front-End - June 2024/11. DOM Manipulation and Events - Lab/06.JS-Front-End-DOM-Events-Resources/03. Add-Delete/app.js
@@ -1,3 +1,23 @@
 function addItem() {
-    //TODO...
+  const itemsElement = document.getElementById('items');
+  const newItemTextElement = document.getElementById('newItemText');
+
+  const liElement = document.createElement('li');
+  liElement.textContent = newItemTextElement.value;
+
+  const deleteButton = document.createElement('a');
+  deleteButton.textContent = '[Delete]';
+  //   deleteButton.setAttribute('href', '#');
+  deleteButton.href = '#';
+
+  deleteButton.addEventListener('click', (e) => {
+    // liElement.remove(); NO!
+    e.currentTarget.parentElement.remove(); //currentTarget e елемента, който сме задали "Delete бутона". Достъпваме елемента родител "li" и го изтриваме целия;
+  });
+
+  liElement.appendChild(deleteButton);
+
+  itemsElement.appendChild(liElement);
+
+  newItemTextElement.value = '';
 }
