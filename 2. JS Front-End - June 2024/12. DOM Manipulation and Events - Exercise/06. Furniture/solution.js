5ef44127f0496ae6db579237e72d256e0fd8abe5
YEnchev <ysenchev@gmail.com>
2025-03-28 23:28:34 +0200

    DOM

--- 2. JS Front-End - June 2024/12. DOM Manipulation and Events - Exercise/06. Furniture/solution.js
+++ 2. JS Front-End - June 2024/12. DOM Manipulation and Events - Exercise/06. Furniture/solution.js
@@ -1,4 +1,36 @@
 function solve() {
   const [generateBtn, buyBtn] = document.querySelectorAll('button');
   const [inputTextarea, outputTextarea] = document.querySelectorAll('textarea');
+
+  const tBodyElement = document.querySelector('tbody');
+  const firstTrElement = tBodyElement.querySelector('tr');
+
+  function appendTrForEachData({ img, name, price, decFactor }) {
+    const currentTrClone = firstTrElement.cloneNode(true);
+
+    // changing the image
+    currentTrClone.children[0].children[0].setAttribute('src', img);
+
+    // changing the name
+    currentTrClone.children[1].children[0].textContent = name;
+
+    // changing the price
+    currentTrClone.children[2].children[0].textContent = price;
+
+    // changing the decFactor
+    currentTrClone.children[3].children[0].textContent = decFactor;
+
+    //enable the checkbox
+    currentTrClone.children[4].children[0].disabled = false;
+
+    tBodyElement.appendChild(currentTrClone);
+  }
+
+  function onGenerateBtnClickHandler() {
+    const inputData = JSON.parse(inputTextarea.value);
+
+    inputData.forEach(appendTrForEachData);
+  }
+
+  generateBtn.addEventListener('click', onGenerateBtnClickHandler);
 }
