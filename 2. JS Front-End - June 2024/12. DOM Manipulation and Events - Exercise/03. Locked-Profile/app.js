75121b0309b2adeb467eaa7dde66e9af7ab6c40e
YEnchev <ysenchev@gmail.com>
2025-04-02 00:33:48 +0300

    DOM

--- 2. JS Front-End - June 2024/12. DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
+++ 2. JS Front-End - June 2024/12. DOM Manipulation and Events - Exercise/03. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+  const buttonElements = document.querySelectorAll('div.profile button');
+
+  function showMoreClickHandler(event) {
+    const button = event.target;
+    const [, unlockInputElement] =
+      button.parentElement.querySelectorAll('input');
+
+    if (!unlockInputElement.checked) {
+      return;
+    }
+
+    const hiddenDataElement = button.parentElement.querySelector('div');
+
+    if (button.textContent === 'Show more') {
+      hiddenDataElement.style.display = 'block';
+      button.textContent = 'Hide it';
+    } else {
+      hiddenDataElement.style.display = 'none';
+      button.textContent = 'Show more';
+    }
+  }
+
+  buttonElements.forEach((buttonElement) =>
+    buttonElement.addEventListener('click', showMoreClickHandler)
+  );
 }
